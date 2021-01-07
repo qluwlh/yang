6e57649e1a529174d5933b44811ef6448fe54d71
wanglihua <1106990976@qq.com>
2021-01-07 20:55:00 +0800

    [add]: download file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import Excel from 'exceljs'
+import moment from 'moment'
 import os from 'os'
 import path from 'path'
-import { awaitTime, createDirectory, getData, getInitData, handleData, listPublicViewDataGrid } from './util'
-
+import { awaitTime, createDirectory, getData, getInitData, handleData, listFeedBackGrid, listPublicViewDataGrid } from './util'
 const filePath = path.resolve(os.homedir(), 'Documents', 'yang')
-const excelFilename = `${filePath}/demo.xlsx`
+
+const excelFilename = `${filePath}/demo-${moment().format('YYYY-MM-DD_HH:mm')}.xlsx`
+
+const types = {
+  A0: '初稿',
+  L0: '上会搞',
+  D0: '终稿',
+}
 
 const options = {
   filename: excelFilename,
@@ -13,11 +20,15 @@ const options = {
 }
 const columns = [
   { header: 'instNo', key: 'instNo', width: 20 },
-  { header: 'regFileName', key: 'regFileName', width: 60 },
-  { header: 'releaseTime', key: 'releaseTime', width: 30 },
+  { header: '债券名称', key: 'regFileName', width: 60 },
+  { header: '更新日期', key: 'releaseTime', width: 30 },
   { header: 'projTrackNo', key: 'projTrackNo', width: 40 },
-  { header: 'regNoticeNo', key: 'regNoticeNo', width: 30 },
-  { header: 'fileName', key: 'fileName', width: 30 },
+  { header: '注册通知书文号', key: 'regNoticeNo', width: 30 },
+  { header: '初稿', key: 'firstDraft', width: 30 },
+  { header: '终稿', key: 'finalDraft', width: 30 },
+  { header: '注册报告', key: 'registrationStatement', width: 30 },
+  { header: '问询函', key: 'inquiryLetter', width: 30 },
+  { header: '回函', key: 'replyLetter', width: 30 },
 ]
 const run = async () => {
   const initData = await getInitData()
@@ -38,27 +49,52 @@ const run = async () => {
     for (const currentData of data) {
       const row = currentData
       const publicViewDataGrids = await listPublicViewDataGrid(currentData.instNo, currentData.projTrackNo)
-      for (const item of publicViewDataGrids) {
-        const { storeLocation, fileName } = item
-        if (storeLocation) {
-          const fileIds = storeLocation.split(',')
-          if (fileIds && fileIds.length > 0) {
-            row.fileName = JSON.stringify({
-              url: 'http://zhuce.nafmii.org.cn/file_web/file/download',
-              opt: { params: { bo: { fileName, fileId: fileIds[0] } } },
-            })
+      if (publicViewDataGrids && publicViewDataGrids.length > 0) {
+        const prospectus = publicViewDataGrids.filter(({ fileName }) => /募集说明书/.test(fileName))
+        if (prospectus && prospectus.length > 0) {
+          for (const item of prospectus) {
+            const { storeLocation, fileName, versionType } = item
+            if (storeLocation) {
+              const fileIds = storeLocation.split(',')
+              const fileTypes = versionType.split(',')
+              const files = fileTypes.map((item, index) => ({ type: item, fileId: fileIds[index] }))
+              const firstDraft = files.find((item) => item.type === types.A0)
+              const finalDraft = files.find((item) => item.type === types.D0)
+              if (firstDraft) {
+                row.firstDraft = JSON.stringify({
+                  url: 'http://zhuce.nafmii.org.cn/file_web/file/download',
+                  opt: { params: { bo: { fileName, fileId: firstDraft.fileId } } },
+                })
+              }
+              if (finalDraft) {
+                row.finalDraft = JSON.stringify({
+                  url: 'http://zhuce.nafmii.org.cn/file_web/file/download',
+                  opt: { params: { bo: { fileName, fileId: finalDraft.fileId } } },
+                })
+              }
+            }
+            //  await downFile('http://zhuce.nafmii.org.cn/file_web/file/download',filePath,fileName,fileIds[0])
           }
         }
-        //  await downFile('http://zhuce.nafmii.org.cn/file_web/file/download',filePath,fileName,fileIds[0])
+        const registrationStatement = publicViewDataGrids.filter(({ fileName }) => /注册报告/.test(fileName))
       }
-      row.fileName && console.log('row', JSON.parse(row.fileName))
+      const feedBackDataGrids = await listFeedBackGrid(currentData.instNo, currentData.projTrackNo)
+      if (feedBackDataGrids && feedBackDataGrids.length > 0) {
+        const [inquiryLetter, replyLetter] = feedBackDataGrids
+        if (inquiryLetter) {
+          row.inquiryLetter = JSON.stringify({
+            url: 'http://zhuce.nafmii.org.cn/file_web/file/download',
+            opt: { params: { bo: { fileName: inquiryLetter.fileName, fileId: inquiryLetter.storeLocation } } },
+          })
+        }
+        if (replyLetter) {
+          row.replyLetter = JSON.stringify({
+            url: 'http://zhuce.nafmii.org.cn/file_web/file/download',
+            opt: { params: { bo: { fileName: replyLetter.fileName, fileId: replyLetter.storeLocation } } },
+          })
+        }
+      }
       worksheet.addRow(row).commit()
-      // for (const item of publicViewDataGrids) {
-      //    const {storeLocation,fileName} = item
-      //    fileIds = storeLocation.split(',')
-      //    await awaitTime(1000)
-      //    await downFile('http://zhuce.nafmii.org.cn/file_web/file/download',filePath,fileName,fileIds[0])
-      // }
     }
   }
   worksheet.commit()
--- src/util.js
+++ src/util.js
@@ -1,143 +1,145 @@
-const util = require('util');
-const fs = require('fs');
-const axios = require('axios');
-export const access = util.promisify(fs.access);
-export const mkdir = util.promisify(fs.mkdir);
+const util = require('util')
+const fs = require('fs')
+const axios = require('axios')
+export const access = util.promisify(fs.access)
+export const mkdir = util.promisify(fs.mkdir)
 
 export const awaitTime = (times = 2000) => {
-  return new Promise((resolve,reject) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve()
-    },times)
+    }, times)
   })
 }
 
-
-export const downFile = (url, filePath,fileName,fileId) => {
-  return axios.get(url,{params:{bo:{fileName,fileId}},
-    responseType: "stream",
-  }).then((res)=> {
-     const writer=fs.createWriteStream(path.resolve(filePath,fileName));
-     res.data.pipe(writer);
-     console.log(`✅ ${fileName},  ✌️ 位置：${path.resolve(filePath)}`)
-  }).catch((e) => {
-    console.log(`❌ ${fileName} 下载失败, 错误：${e}`)
-  })
+export const downFile = (url, filePath, fileName, fileId) => {
+  return axios
+    .get(url, { params: { bo: { fileName, fileId } }, responseType: 'stream' })
+    .then((res) => {
+      const writer = fs.createWriteStream(path.resolve(filePath, fileName))
+      res.data.pipe(writer)
+      console.log(`✅ ${fileName},  ✌️ 位置：${path.resolve(filePath)}`)
+    })
+    .catch((e) => {
+      console.log(`❌ ${fileName} 下载失败, 错误：${e}`)
+    })
 }
 
-export const createDirectory = (filePath) =>  new Promise((resolve,reject) => {
-  fs.access(filePath, fs.constants.F_OK, (err) => {
-    if(err){
-      fs.mkdir(filePath, { recursive: true }, (err) => {
-        if (err) {
-          console.log(`创建失败`);
-          reject(err)
-        }else{
-          resolve(true)
-        }
-      });
-    }else{
-      resolve(true)
-    }
-  });
-})
+export const createDirectory = (filePath) =>
+  new Promise((resolve, reject) => {
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+      if (err) {
+        fs.mkdir(filePath, { recursive: true }, (err) => {
+          if (err) {
+            console.log(`创建失败`)
+            reject(err)
+          } else {
+            resolve(true)
+          }
+        })
+      } else {
+        resolve(true)
+      }
+    })
+  })
 
-
-export const getData = (pageNo,pageSize) => axios.get('http://zhuce.nafmii.org.cn/fans/publicQuery/releFileProjDataGrid',{
-  params: {
-    page:pageNo,
-    rows:pageSize,
-    startTime: '2013-01-01',
-    endTime: '2019-12-31',
-  }
-}).then((response) => {
-  if(response.status === 200){
-    return response.data
-  }else{
-    throw new Error('接口获取失败')
-  }
-}).then(({rows}) => {
-  return rows
-})
+export const getData = (pageNo, pageSize) =>
+  axios
+    .get('http://zhuce.nafmii.org.cn/fans/publicQuery/releFileProjDataGrid', {
+      params: {
+        page: pageNo,
+        rows: pageSize,
+        startTime: '2013-01-01',
+        endTime: '2019-12-31',
+      },
+    })
+    .then((response) => {
+      if (response.status === 200) {
+        return response.data
+      } else {
+        throw new Error('接口获取失败')
+      }
+    })
+    .then(({ rows }) => {
+      return rows
+    })
 
 export const handleData = (rows) => {
-  return rows.filter(({ regNoticeNo }) =>
-              /\[[1-9]\d{3}\](CP|SCP|MTN|DFI)\d+/.test(regNoticeNo)
-            )
-            .map(
-              ({
-                instNo,
-                regFileName,
-                regNoticeNo,
-                releaseTime,
-                projTrackNo,
-              }) => ({
-                instNo,
-                regFileName,
-                releaseTime,
-                projTrackNo,
-                regNoticeNo,
-              })
-            )
+  return rows
+    .filter(({ regNoticeNo }) => /\[[1-9]\d{3}\](CP|SCP|MTN|DFI)\d+/.test(regNoticeNo))
+    .map(({ instNo, regFileName, regNoticeNo, releaseTime, projTrackNo }) => ({
+      instNo,
+      regFileName,
+      releaseTime,
+      projTrackNo,
+      regNoticeNo,
+    }))
 }
 
 //反馈意见及回复
-export const listFeedBackGrid = (instNo,projTrackNo) =>{
-  return axios.get('http://zhuce.nafmii.org.cn/fans/publicQuery/feedBackGrid',{
-    params: {
-      instNo,
-      projTrackNo
-    }
-  }).then((response) => {
-    if(response.status === 200){
-      return response.data
-    }else{
-      throw new Error('接口获取失败')
-    }
-  }).then(({rows}) => {
-    const a = {"webUrl":",,","releaseUser":"138867","submitTime":"2020-09-14 17:17:34","toRelease":"1","instNo":"50000003359874","uploadUser":"124628","releaseUserName":"侯晓霞","submitProcNo":"60000003358951","uploadProcNo":"60000003342848","storeLocation":"357f921bb07744e91713165d33f6ea9e^!$1795155,357f921bb07744e91713165d33f6ea9e^!$1795155,357f921bb07744e91713165d33f6ea9e^!$1795155","uploadTime":"2020-09-09 18:38:03","submitUser":"133827","submitUserName":"潘鑫","projTrackNo":"AAC715C0375E0088E0530A0110024DC2","releaseProcNo":"50000003381156","itemType":"07","fileName":"华夏银行股份有限公司关于推荐知识城（广州）投资集团有限公司注册发行中期票据的函.pdf","versionType":"A0,L0,D0","releaseTime":"2020-12-25 17:34:53","uploadUserName":"张琪悦"};
-    return rows
-  })
+export const listFeedBackGrid = (instNo, projTrackNo) => {
+  return axios
+    .get('http://zhuce.nafmii.org.cn/fans/publicQuery/feedBackGrid', {
+      params: {
+        instNo,
+        projTrackNo,
+      },
+    })
+    .then((response) => {
+      if (response.status === 200 && response.data && response.data.rows) {
+        return response.data
+      } else {
+        console.log(`反馈意见及回复接口data获取失败--instNo: ${instNo},projTrackNo: ${projTrackNo}`)
+        return { rows: [] }
+      }
+    })
+    .then(({ rows }) => {
+      return rows
+    })
+    .catch((error) => {
+      console.log(`error 反馈意见及回复接口调用：${error}`)
+      return []
+    })
 }
 
 //公开披露文件
-export const listPublicViewDataGrid = (instNo,projTrackNo) =>{
-  return axios.get('http://zhuce.nafmii.org.cn/fans/publicQuery/publicViewDataGrid',{
-    params: {
-      instNo,
-      projTrackNo
-    }
-  }).then((response) => {
-    if(response.status === 200){
-      if(response.data && response.data.rows){
+export const listPublicViewDataGrid = (instNo, projTrackNo) => {
+  return axios
+    .get('http://zhuce.nafmii.org.cn/fans/publicQuery/publicViewDataGrid', {
+      params: {
+        instNo,
+        projTrackNo,
+      },
+    })
+    .then((response) => {
+      if (response.status === 200 && response.data && response.data.rows) {
         return response.data
-      }else{
-        console.log( `data获取失败--instNo: ${instNo},projTrackNo: ${projTrackNo}`)
-        return {rows:[]}
+      } else {
+        console.log(`公开披露文件data获取失败--instNo: ${instNo},projTrackNo: ${projTrackNo}`)
+        return { rows: [] }
       }
-    }else{
-      throw new Error('接口获取失败')
-    }
-  }).then(({rows}) => {
-    const a = {"webUrl":",,","releaseUser":"138867","submitTime":"2020-09-14 17:17:34","toRelease":"1","instNo":"50000003359874","uploadUser":"124628","releaseUserName":"侯晓霞","submitProcNo":"60000003358951","uploadProcNo":"60000003342848","storeLocation":"357f921bb07744e91713165d33f6ea9e^!$1795155,357f921bb07744e91713165d33f6ea9e^!$1795155,357f921bb07744e91713165d33f6ea9e^!$1795155","uploadTime":"2020-09-09 18:38:03","submitUser":"133827","submitUserName":"潘鑫","projTrackNo":"AAC715C0375E0088E0530A0110024DC2","releaseProcNo":"50000003381156","itemType":"07","fileName":"华夏银行股份有限公司关于推荐知识城（广州）投资集团有限公司注册发行中期票据的函.pdf","versionType":"A0,L0,D0","releaseTime":"2020-12-25 17:34:53","uploadUserName":"张琪悦"};
-    return rows
-  })
+    })
+    .then(({ rows }) => {
+      return rows
+    })
+    .catch((error) => {
+      console.log(`error:公开披露文件接口调用失败 ${error}`)
+      return []
+    })
 }
 
-
-
-const params =  {
-  page:1,
-  rows:1,
+const params = {
+  page: 1,
+  rows: 1,
   startTime: '2013-01-01',
   endTime: '2019-12-31',
 }
 
 export const getInitData = async () => {
-  const response = await axios.get('http://zhuce.nafmii.org.cn/fans/publicQuery/releFileProjDataGrid',{params})
-  if(!response || response.status !== 200){
+  const response = await axios.get('http://zhuce.nafmii.org.cn/fans/publicQuery/releFileProjDataGrid', { params })
+  if (!response || response.status !== 200) {
     throw new Error('接口获取失败')
-  }else{
+  } else {
     return response.data
   }
 }
